196e4945aec5cbaf35365e84734c040237f4565d
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-13 11:40:22 +0200

    refactor: update postControllers to improve response handling and remove commented code

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,16 +1,29 @@
+const posts = require(`../data/posts`);
 
 function index (req, res){
-    res.send(`Welcome to mi page`);
+    //res.send(`Welcome to mi page`);
+    res.json(posts)
 }
 
 function show (req, res){
-    res.send(`showing the Element`)
+    //res.send(`showing the Element`)
+    // devo cercare un elemento e visualizzarlo, possom usare il find per trovarlo
+    const {id} = req.params
+    const post = posts.find(item => item.id === parseInt(id));
+    
+    if(!post){
+        res.status(404).json({
+            error: true,
+            message: 'Resource not found'
+            
+        })
+    }
+    res.json(post);
 }
 
 function store (req, res){
     res.send(`created a new element`);
 }
-
 
 function update (req, res){
     res.send(`updating the element`);
@@ -21,7 +34,21 @@ function modify (req, res){
 }
 
 function destroy (req, res){
-    res.send(`deleting the element`);
+    //res.send(`deleting the element`);
+    const {id} = req.params
+    const post = posts.find(item => item.id === parseInt(id));
+
+     if(!post){
+        res.status(404).json({
+            error: true,
+            message: 'Resource not found'
+            
+        })
+    }
+    posts.splice(posts.indexOf(post),1);
+    res.sendStatus(204);
+    console.log(posts);
+
 }
 
 module.exports = {
